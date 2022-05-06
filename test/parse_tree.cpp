ccb070810b5086ba4d3293b3aa6bd3efb10f7f26
Carmine Margiotta <car.margiotta@icloud.com>
2022-05-06 18:29:09 +0200

    Tested parse tree

--- test/parse_tree.cpp
+++ test/parse_tree.cpp
@@ -1,35 +1,86 @@
 #include <catch2/catch.hpp>
 
+#include <fstream>
 #include <iostream>
 
-#include "parser/parse_node.hpp"
-#include "parser/parser.hpp"
-#include "parser/tokenizer/exact_tokenizer.hpp"
-#include "stubs/parse_node.hpp"
+#include <parser/math.hpp>
+#include <parser/regex.hpp>
 
-SCENARIO("Parse tree realization")
+SCENARIO("Regex parse tree")
 {
-    GIVEN("A simple parse node")
+    GIVEN("A regex parser and an expression tree")
     {
-        WHEN("A parser is declared with the node inside")
-        {
-            ratl::parser<ratl::tokenizer::exact_tokenizer<char>, node> parser;
+        ratl::regex_parser parser_;
 
-            THEN("No errors occur")
+        std::string parse_expr("1*2+a?[\\dA]*");
+        auto        tree = parser_(parse_expr);
+
+        std::string parse_expr1("[1-2]+");
+        auto        tree1 = parser_(parse_expr1);
+
+        REQUIRE(tree->to_string() == parse_expr);
+        REQUIRE(tree1->to_string() == parse_expr1);
+
+        WHEN("It is asked to compute")
+        {
+            THEN("It correctly interprets its language")
             {
+                std::string expression("1222");
+                REQUIRE(tree->compute(expression));
+
+                expression = "22222";
+                REQUIRE(tree->compute(expression));
+
+                expression = "1111122222";
+                REQUIRE(tree->compute(expression));
+
+                expression = "122222a";
+                REQUIRE(tree->compute(expression));
+
+                expression = "122222a9999";
+                REQUIRE(tree->compute(expression));
+
+                expression = "122222aAAAA";
+                REQUIRE(tree->compute(expression));
+
+                expression = "122222a9AA999";
+                REQUIRE(tree->compute(expression));
+
+                expression = "22222aa";
+                REQUIRE(tree->compute(expression));
+
+                expression = "111aa";
+                REQUIRE(!tree->compute(expression));
+
+                expression = "111";
+                REQUIRE(!tree->compute(expression));
+
+                expression = "";
+                REQUIRE(!tree->compute(expression));
+
+                expression = "12344";
+                REQUIRE(tree1->compute(expression));
             }
         }
-        AND_WHEN("The built parser is called for an expression")
+    }
+}
+
+SCENARIO("Math parse tree")
+{
+    GIVEN("A regex parser and an expression tree")
+    {
+        ratl::math_parser parser_;
+
+        std::string parse_expr("12+5");
+        auto        tree = parser_(parse_expr);
+
+        REQUIRE(tree->to_string() == parse_expr);
+
+        WHEN("It is asked to compute")
         {
-            ratl::parser<ratl::tokenizer::exact_tokenizer<char>, node> parser;
-
-            std::string expression("a");
-
-            auto tree = parser(expression.begin(), expression.end());
-
-            THEN("The built parse tree is correct")
+            THEN("It correctly interprets its expression")
             {
-                REQUIRE(tree->get_identifier() == 'a');
+                REQUIRE(tree->compute() == 17);
             }
         }
     }
